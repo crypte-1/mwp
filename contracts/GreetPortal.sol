872f864cc62fed8300d2fed6d1489c2b847336da
crypte-1 <renukavutukuri1@gmail.com>
2022-06-04 10:49:43 +0530

    Modified

--- contracts/GreetPortal.sol
+++ contracts/GreetPortal.sol
@@ -4,8 +4,20 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract GreetPortal {
+contract WavePortal {
+    uint256 totalGreet;
+
     constructor() {
-        console.log("Yo yo, I am a Smart-contract and Welcome to GreetPortal");
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function Greet() public {
+        totalGreet += 1;
+        console.log("%s has Greeted!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total Greetes!", totalGreet);
+        return totalGreet;
     }
 }
